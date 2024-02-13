69f079f1a771eaa6280e8fc8ac7b1940ccaf3c30
Protengstar <musthofasifaulq@gmail.com>
2024-02-13 11:51:49 +0700

    PENGULANGAN : TABEL PENELUSURAN

--- PENGULANGAN  WHILE pada JAVASCRIPT/script.js
+++ PENGULANGAN  WHILE pada JAVASCRIPT/script.js
@@ -15,8 +15,8 @@
 // nilaiAwal++;
 // }
 
-var nilaiAwal = 1;
-while(nilaiAwal <= 50) {
-    console.log('hello world!' + nilaiAwal + 'x');
-nilaiAwal++;
-}
+// var nilaiAwal = 1;
+// while(nilaiAwal <= 50) {
+//     console.log('hello world!' + nilaiAwal + 'x');
+// nilaiAwal++;
+// }
