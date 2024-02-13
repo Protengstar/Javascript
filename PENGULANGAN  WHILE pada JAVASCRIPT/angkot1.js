13625ece6ac14eab7a20ef8386b66342a79e837c
Protengstar <musthofasifaulq@gmail.com>
2024-02-13 10:35:58 +0700

    [200~var nilaiAwal = 1;

---
+++ PENGULANGAN  WHILE pada JAVASCRIPT/angkot1.js
@@ -0,0 +1,5 @@
+var nilaiAwal = 1;
+while(nilaiAwal <= 10) {
+    console.log('angkot no.' + nilaiAwal + ' beroprasi dengan baik.');
+nilaiAwal++;
+}
---
+++ PENGULANGAN  WHILE pada JAVASCRIPT/script.js
@@ -0,0 +1,22 @@
+// INFINITE LOOP
+// while(false) {
+//     console.log('hello world!');
+// }
+
+// var ulang = true;
+// while(ulang) {
+//     console.log('hello world!');
+//     ulang = confirm ('again?');
+// }
+
+// var nilaiAwal = 1;
+// while(nilaiAwal <= 5) {
+//     console.log('hello world!');
+// nilaiAwal++;
+// }
+
+var nilaiAwal = 1;
+while(nilaiAwal <= 50) {
+    console.log('hello world!' + nilaiAwal + 'x');
+nilaiAwal++;
+}
