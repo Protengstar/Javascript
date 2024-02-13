cd2086d92e6809a97025bcc77e0713ff0bb7b4c1
Protengstar <musthofasifaulq@gmail.com>
2024-02-13 09:57:25 +0700

    control flow

---
+++ CONTROL FLOW pada JAVASCRIPT/script.js
@@ -0,0 +1,13 @@
+// alert('mulai');
+// for(var i = 0; i < 5; i++) {
+//     alert('Hello Wolrd!');
+// }
+// alert('selesai');
+
+var angka = prompt('masukkan angka:');
+
+if( angka % 2 === 0 ) {
+    alert(angka + 'adalah bilangan GENAP');
+} else {
+    alert(angka + 'adalah bilangan GANJIL');
+}
