a7f12c7c0f9cc36902c3684de8feb1f2ce440820
Protengstar <musthofasifaulq@gmail.com>
2024-02-14 23:22:25 +0700

    PENGKONDISIAN : ELSE IF & IF ELSE

--- 7.PENGKONDISIAN  IF ELSE/angkot3.js
+++ 7.PENGKONDISIAN  IF ELSE/angkot3.js
@@ -1,10 +1,21 @@
-var jmlAngkot = 10;
-var angkotBeroprasi = 6;
+// IF ELSE
+// var jmlAngkot = 10;
+// var angkotBeroprasi = 6;
 
-for( var noAngkot = 1; noAngkot <= jmlAngkot; noAngkot++ ) {
-    if( noAngkot <= angkotBeroprasi ) {
-        console.log('angkot no. ' + noAngkot + 'sedang beroprasi')
-    } else {
-        console.log('angkot no. ' + noAngkot + 'sedang rusak')
-    }
+// for( var noAngkot = 1; noAngkot <= jmlAngkot; noAngkot++ ) {
+//     if( noAngkot <= angkotBeroprasi ) {
+//         console.log('angkot no. ' + noAngkot + 'sedang beroprasi')
+//     } else {
+//         console.log('angkot no. ' + noAngkot + 'sedang rusak')
+//     }
+// }
+
+// ELSE IF
+var angka = prompt('Masukkan angka:')
+if( angka % 2 == 0) {
+    alert(angka + ' ini adalah bilangan GENAP');
+} else if( angka % 2 == 1) {
+    alert(angka + ' Ini adalah bilangan GANJIL');
+} else {
+    alert(' ini adalah HURUF');
 }
