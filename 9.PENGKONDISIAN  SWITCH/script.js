e7cc4d683df2beeefea99ce1f2fe37bfdb4d0beb
Protengstar <musthofasifaulq@gmail.com>
2024-02-15 13:23:55 +0700

    PENGULANGAN & PENGKONDISIAN BERSARANG

--- 9.PENGKONDISIAN  SWITCH/script.js
+++ 9.PENGKONDISIAN  SWITCH/script.js
@@ -1,14 +1,27 @@
-var item = prompt('masukkan nama makanan / minuman : \n (contoh: nasi, daging, susu, mie, gorengan, soda )');
+// var item = prompt('masukkan nama makanan / minuman : \n (contoh: nasi, daging, susu, mie, gorengan, soda )');
 
-switch( item ) {
-    case'nasi':
-    case'daging':
-    case'susu':
-        alert('makanan / minuman SEHAT');
-        break;
-    case'mie':
-    case'gorengan':
-    case'soda':
-        alert('makanan / minuman TIDAK SEHAT');
-        break;
+// switch( item ) {
+//     case'nasi':
+//     case'daging':
+//     case'susu':
+//         alert('makanan / minuman SEHAT');
+//         break;
+//     case'mie':
+//     case'gorengan':
+//     case'soda':
+//         alert('makanan / minuman TIDAK SEHAT');
+//         break;
+// }
+
+var s = '';
+for( var i = 10; i > 0; i-- ) {
+    for( var j = 0; j < i; j++ ) {
+        s += '# ';
+    }
+    s += '\n';
+    for( var j = 0; j < i; j++ ) {
+        s += ' #';
+    }
+    s += '\n';
 }
+console.log(s);
