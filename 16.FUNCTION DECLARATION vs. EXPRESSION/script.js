9a4b406c8d20bfcbe6f92abf3fa1da31bb38fe9e
Protengstar <musthofasifaulq@gmail.com>
2024-02-16 15:27:37 +0700

    FUNCTION DECLARATION vs. EXPRESSION

---
+++ 16.FUNCTION DECLARATION vs. EXPRESSION/script.js
@@ -0,0 +1,13 @@
+// DECLARATION
+// function pesan (nama) {
+//     alert('halo' + nama);
+// }
+
+// pesan('Hello world');
+
+// EXPRESSION
+var pesan = function (nama) {
+    alert('halo' + nama);
+}
+
+pesan('Hello world');
