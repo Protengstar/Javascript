1ff15cbde355b143efed9aaf714c480c9fa71c3a
Protengstar <musthofasifaulq@gmail.com>
2024-02-16 08:46:29 +0700

    Function membuat dan menjalankan function

---
+++ 11.FUNCTION Membuat dan Menjalankan FUNCTION/script.js
@@ -0,0 +1,15 @@
+function jumlahVolumeDuaKubus(a, b) {
+    var volumeA;
+    var voluneB;
+    var total;
+
+    volumeA = a * a * a;
+    voluneB = b * b * b;
+
+    total = volumeA + voluneB;
+
+    return total;
+}
+
+alert(jumlahVolumeDuaKubus(8, 3));
+alert(jumlahVolumeDuaKubus(5, 10));
