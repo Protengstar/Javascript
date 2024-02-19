3b793811b73994a0bb7f219b59131982f6052c68
Protengstar <musthofasifaulq@gmail.com>
2024-02-19 15:35:13 +0700

    MEMBUAT PROGRAM PENGELOLAAN PENUMPANG ANGKOT dengan JAVASCRIPT PART1

--- 21.MEMBUAT PROGRAM PENGELOLAAN PENUMPANG ANGKOT dengan JAVASCRIPT PART1/angkot.js
+++ 21.MEMBUAT PROGRAM PENGELOLAAN PENUMPANG ANGKOT dengan JAVASCRIPT PART1/angkot.js
@@ -1,17 +1,17 @@
-var penumpang = ['budi', undefined, 'joko'];
-var tambahPenumpang = function(namaPenumpang, penumpang) {
-    // JIKA ANGKOT KOSONG
+var penumpang = ['joko', undefined, 'reno'];
+var penumpangNaik = function(namaPenumpang, penumpang) {
+    // SAAT ANGKOT KOSONG
     if( penumpang.length == 0 ) {
-        // TAMBAH PENUMPANG DI AWAL ARRAY
+        // TAMBAHKAN PENUMPANG DI AWAL ARRAY
         penumpang.push(namaPenumpang);
         // KEMBALIKAN ISI ARRAY & KELUAR DARI FUNCTION
         return penumpang;
     } else {
         // TELUSURI SELURUH KURSI DARI AWAL
-        for( var i = 0; i < penumpang.length; i++ ) {
-            // JIKA ADA KURSI KOSONG
+        for( var i = 0; i < penumpang.length; i++) {
+            // JIKA KURSI KOSONG
             if( penumpang[i] == undefined ) {
-                // TAMBAH PENUMPANG DI KURSI TERSEBUT
+                // TAMBAHKAN PENUMPANG DI KURSI TERSEBUT
                 penumpang[i] = namaPenumpang;
                 // KEMBALIKAN KURSI ARRAY & KELUAR DARI FUNCTION
                 return penumpang;
@@ -19,17 +19,16 @@ var tambahPenumpang = function(namaPenumpang, penumpang) {
             // JIKA SUDAH ADA NAMA YANG SAMA
             else if( namaPenumpang == penumpang[i] ) {
                 // PESAN KESALAHAN
-                console.log(namaPenumpang + ' sudah ada didalam angkot.');
+                console.log(namaPenumpang + ' sudah ada di dalam angkot.');
                 // KEMBALIKAN ISI ARRAY & KELUAR DARI FUNCTION
                 return penumpang;
             }
-            else if( i == penumpang.length - 1 ) {
-                // TAMBAH PENUMPANG DI AKHIR ARRAY
+            else if( i == penumpang.length -1 ) {
+                // TAMBAHKAN PENUMPANG DI AKHIR ARRAY
                 penumpang.push(namaPenumpang);
                 // KEMBALIKAN ISI ARRAY & KELUAR DARI FUNCTION
                 return penumpang;
-                
             }
         }
     }
-}
+} 
