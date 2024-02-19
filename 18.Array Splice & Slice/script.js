f8ab165402e9c7002fe9812b88a84dcbb710150b
Protengstar <musthofasifaulq@gmail.com>
2024-02-19 10:06:00 +0700

    Array Splice & Slice

---
+++ 18.Array Splice & Slice/script.js
@@ -0,0 +1,10 @@
+// 4. SPLICE
+    // splice (indexAwal, MauDihapusBerapa?, elemenBaru1, elemenBaru2, ... )
+    // arr.splice(2, 2, 'fana', 'oteng');
+    // console.log(arr.join(' - '));
+    
+    // 5. SLICE
+    var arr = ["lukman", "sifaul", "nico", "farel"];
+    var arr2 = arr.slice(1,3);
+    console.log(arr2.join(' - '));
+    console.log(arr.join(' - '));
