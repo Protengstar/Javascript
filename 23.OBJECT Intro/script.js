d37d76ff260eebae4a1e7bc656562e158ba6cb29
Protengstar <musthofasifaulq@gmail.com>
2024-02-19 16:14:11 +0700

    OBJECT Intro

---
+++ 23.OBJECT Intro/script.js
@@ -0,0 +1,10 @@
+var mhs = {
+    nama : "MUSTHOFA SIFA'UL Q",
+    umur : "18thn",
+    ips : [9.8, 9.4, 9.9],
+    alamat : {
+        jalan : "Jl.agus salim",
+        kota : "Jember",
+        Provinsi : "Jawa Timur"
+    }
+};
