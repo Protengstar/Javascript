ac9528a996aeb2aff62555aca8e7631e64e536a0
Protengstar <musthofasifaulq@gmail.com>
2024-02-19 09:36:08 +0700

    ARRAY manipulasi array

---
+++ 17.Array manipulasi array/script.js
@@ -0,0 +1,45 @@
+// MANIPULASI ARRAY
+
+// 1. MENAMBAH ISI ARRAY
+
+    // A.
+    // var arr = ["a", 1, false];
+    // console.log(arr[1]);
+
+    // B.
+    // var arr = [];
+    // arr [0] = "lukman";
+    // arr [1] = "sifaul";
+    // arr [2] = "nico";
+    // arr [5] = "farel";
+
+    // console.log(arr);
+
+// 2. MENGHAPUS ISI ARRAY
+    // var arr = ["lukman", "sifaul", "nico"];
+    // arr[2] = undefined;
+    // console.log(arr);
+
+// 3. MENAMPILKAN ISI ARRAY
+    // var arr = ["lukman", "sifaul", "nico", "farel"];
+    // for( var i = 0; i < arr.length; i++) {
+        //     console.log('Mahasiswa ke-' + (i+1) + ' : ' + arr[i]);
+        // }
+        
+        
+// METHOD PADA ARRAY
+        
+var arr = ["lukman", "sifaul", "nico", "farel"];
+// 1. JOIN
+    // console.log(arr.join(' - '));
+
+// 2. PUSH & POP
+    // arr.push('dimas', 'oteng');
+    // arr.pop();
+    // arr.pop();
+    // console.log(arr.join(' - '));
+
+// 3.UNSHIFT & SHIFT
+    // arr.unshift('fana');
+    arr.shift();
+    console.log(arr.join(' - '));
