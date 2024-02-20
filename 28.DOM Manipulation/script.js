6075554ddc891f964a0de335c4f057a48b1cb1f3
Protengstar <musthofasifaulq@gmail.com>
2024-02-20 14:20:41 +0700

    DOM Manipulation Part2

--- 28.DOM Manipulation/script.js
+++ 28.DOM Manipulation/script.js
@@ -41,3 +41,68 @@ p2.classList.replace('satu', 'empat');
 
 
 
+// BUAT ELEMEN BARU
+const pBaru = document.createElement('p');
+const textPBaru = document.createTextNode('Paragraf Baru');
+// SIMPAN TULISAN KEDALAM PARAGRAF BARU
+pBaru.appendChild(textPBaru);
+// SIMPAN P BARU DIAKHIR SECTION A
+const sectionA = document.getElementById('a');
+sectionA.appendChild(pBaru);
+
+
+
+
+
+
+
+// BUAT LI BARU
+const liBaru = document.createElement('li');
+const textLiBaru = document.createTextNode('item baru');
+// SIMPAN TULISAN KEDALAM LIST BARU
+liBaru.appendChild(textLiBaru);
+
+const ul = document.querySelector('section#b ul');
+const li2 = ul.querySelector('li:nth-child(2)');
+
+ul.insertBefore(liBaru, li2);
+
+
+
+
+
+
+// MENGHAPUS A PADA SECTION-A
+const link = document.getElementsByTagName('a')[0];
+
+sectionA.removeChild(link);
+
+
+
+
+
+
+
+// MENGGANTI NAMA PARAGRAF4 MENJADI H2
+const sectionB = document.getElementById('b');
+const p4 = sectionB.querySelector('p');
+
+const h2Baru = document.createElement('h2');
+const teksH2Baru = document.createTextNode('Judul Baru!');
+
+h2Baru.appendChild(teksH2Baru);
+
+sectionB.replaceChild(h2Baru, p4);
+
+
+
+
+
+
+
+// BARU DITAMBAHKAN
+pBaru.style.backgroundColor = 'cyan';
+liBaru.style.backgroundColor = 'cyan';
+h2Baru.style.backgroundColor = 'cyan';
+
+
