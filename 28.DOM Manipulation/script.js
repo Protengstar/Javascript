8e1adc656a6cc12366a28fea9da2c39e4aa521f1
Protengstar <musthofasifaulq@gmail.com>
2024-02-20 13:38:25 +0700

    DOM Manipulation Part1

---
+++ 28.DOM Manipulation/script.js
@@ -0,0 +1,43 @@
+// const judul = document.getElementById('judul');
+// judul.innerHTML = 'SIFAULLLL';
+
+// const sectionA = document.querySelector('section#a');
+// sectionA.innerHTML = 'info selengkapnya cek instagram @proteng__';
+
+// const judul = document.querySelector('#judul');
+// judul.style.backgroundColor = 'aqua';
+
+// const judul = document.querySelector('h1')[0];
+// judul.setAttribute('name', 'sifaul');
+// const a = document.querySelector('section#a a');
+
+const p2 = document.querySelector('.p2');
+
+// MENAMBAH ClassList
+// p2.classList.add = 'lebel1';
+// p2.classList.add = 'lebel2';
+
+// MENGHAPUS ClassList
+// p2.classList.remove = 'lebel';
+
+// MENAMBAH & MENGHAPUS ClassList
+// p2.classList.toggle = 'lebel';
+
+// MENANYAKAN NOMOR -> ClassList
+// p2.classList.item(1);
+
+// MENANYAKAN NAMA -> ClassList
+// true
+// p2.classList.contains(lebel1);
+// false
+// p2.classList.contains(empat);
+
+// MENGGANTI NAMA ClassList
+p2.classList.replace('satu', 'empat');
+
+
+
+
+
+
+
