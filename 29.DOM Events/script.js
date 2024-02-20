b2f3d6b02277ad35cf1f2919c85ee51c5d59fedd
Protengstar <musthofasifaulq@gmail.com>
2024-02-20 16:00:12 +0700

    DOM Events

---
+++ 29.DOM Events/script.js
@@ -0,0 +1,50 @@
+const p3 = document.querySelector('.p3');
+
+// function ubahWarnaP2() {
+//     p2.style.backgroundColor = 'cyan';
+// }
+// function ubahWarnaP3() {
+//     p3.style.backgroundColor = 'cyan';
+// }
+
+
+// const p2 = document.querySelector('.p2');
+// p2.onclick = ubahWarnaP2;
+
+
+// const p4 = document.querySelector('section#b p');
+// p4.addEventListener('click', function() {
+//     const ul = document.querySelector('section#b ul');
+//     const liBaru = document.createElement('li');
+//     const teksLiBaru = document.createTextNode('item baru');
+
+//     liBaru.appendChild(teksLiBaru);
+//     ul.appendChild(liBaru);
+// });
+
+
+
+// p3.onclick = function() {
+//     p3.style.backgroundColor = 'orange';
+// }
+// p3.onclick = function() {
+//     p3.style.color = 'cyan';
+// }
+// p3.addEventListener('click', function() {
+//     p3.style.backgroundColor = 'orange'
+// });
+// p3.addEventListener('click', function() {
+//     p3.style.color = 'cyan'
+// });
+
+
+p3.addEventListener('mouseenter', function() {
+    p3.style.backgroundColor = 'orange'
+});
+p3.addEventListener('mouseleave', function() {
+    p3.style.backgroundColor = 'white'
+});
+
+
+
+
