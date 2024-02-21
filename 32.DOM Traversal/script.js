8cae13cad3e2065d714f0089393083de318c36a4
Protengstar <musthofasifaulq@gmail.com>
2024-02-21 15:20:54 +0700

    DOM Event Bubbling

--- 32.DOM Traversal/script.js
+++ 32.DOM Traversal/script.js
@@ -8,7 +8,7 @@
 
 
 // DOM Traversal
-const close = document.querySelectorAll('.close');
+// const close = document.querySelectorAll('.close');
 
 // for( let i = 0; i < close.length; i++ ) {
 //     close[i].addEventListener('click', function(e) {
@@ -18,11 +18,24 @@ const close = document.querySelectorAll('.close');
 // }
 
 
-close.forEach(function(el) {
-    el.addEventListener('click', function(e) {
-        e.target.parentElement.style.display = 'none';
-    });
-});
+// close.forEach(function(el) {
+//     el.addEventListener('click', function(e) {
+//         e.target.parentElement.style.display = 'none';
+//         e.stopPropagation();
+//     });
+// });
+
+
+// const cards = document.querySelectorAll('.card');
+// cards.forEach(function(card) {
+//     card.addEventListener('click', function(e) {
+//         alert('ok');
+//     });
+// });
+
+
+
+
 
 
 // const nama = document.querySelector('.nama');
@@ -40,7 +53,13 @@ close.forEach(function(el) {
 
 
 
-
+const container = document.querySelector('.container');
+container.addEventListener('click', function(e) {
+    if( e.target.className == 'close' ) {
+        e.target.parentElement.style.display = 'none';
+        e.preventDefault();
+    }
+});
 
 
 
